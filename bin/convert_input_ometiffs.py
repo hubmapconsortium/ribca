8252d4279f7c99693ec0959fdced299f1aa4d8a0
Matt Ruffalo <matt.ruffalo@gmail.com>
2024-10-17 21:16:20 -0400

    WIP

---
+++ bin/convert_input_ometiffs.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from pathlib import Path
+
+import lxml.etree
+import tifffile
+
+
+def get_channel_names(image: tifffile.TiffFile) -> list[str]:
+    tree = lxml.etree.fromstring(image.ome_metadata.encode("utf-8"))
+    namespaces = tree.nsmap.copy()
+    namespaces["OME"] = namespaces[None]
+    del namespaces[None]
+    channels = tree.xpath("//OME:Pixels/OME:Channel/@Name", namespaces=namespaces)
+    return channels
+
+
+def convert_expr_image(expr_image: Path):
+    f = tifffile.TiffFile(expr_image)
+    channels = get_channel_names(f)
+    with open("marker_list.txt", "w") as f:
+        for c in channels:
+            print(c, f)
+    image_data = f.asarray()
+    squeezed = image_data.squeeze
+    assert len(squeezed.shape) == 3, "Need only CYX dimensions"
+    tifffile.imwrite("expr.tiff", squeezed)
+
+
+def convert_mask_image(mask_image: Path):
+    m = tifffile.TiffFile(mask_image)
+    channels = get_channel_names(m)
+    for i, name in enumerate(channels):
+        if name in {"cell", "cells"}:
+            break
+    else:
+        raise ValueError("No cell channel found in mask")
+    mask_data = m.asarray()
+    squeezed = mask_data.squeeze()
+    cell_mask = squeezed[i]
+    tifffile.imwrite("cell_mask.tiff", cell_mask)
+
+
+if __name__ == "__main__":
+    p = ArgumentParser()
+    p.add_argument("expr_image", type=Path)
+    p.add_argument("mask_image", type=Path)
+    args = p.parse_args()
+
+    convert_expr_image(args.expr_image)
+    convert_mask_image(args.mask_image)

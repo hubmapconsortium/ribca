1f0482dba29774ffa712a52ec8476bda42f0a21d
pennycuda <141153875+pennycuda@users.noreply.github.com>
2025-08-13 12:02:18 -0400

    Add CLID info from HRA crosswalk

--- bin/convert_ribca_output.py
+++ bin/convert_ribca_output.py
@@ -6,21 +6,44 @@ from pathlib import Path
 from shutil import copy, copytree
 
 import pandas as pd
+import json
 from common import cell_type_mapping_filename, find_data_dir
 
 
-def read_cell_type_mapping() -> dict[str, tuple[str, str]]:
-    data_dir = find_data_dir()
-    mapping = {}
-    with open(data_dir / cell_type_mapping_filename, newline="") as f:
-        r = DictReader(f)
-        for d in r:
-            mapping[d["Annotation_Label"]] = d["CL_Label"], d["CL_ID"]
-    return mapping
+def read_clid_mapping():
+    reference = pd.read_csv("/opt/ribca-crosswalk.csv", header=10)
+    label_to_cl_label = dict(zip(reference['Annotation_Label'], reference['CL_Label']))
+    label_to_cl_id = dict(zip(reference['Annotation_Label'], reference['CL_ID']))
+    return label_to_cl_label, label_to_cl_id
+
+
+def map_to_clid(annotations):
+    cl_label_map, cl_id_map = read_clid_mapping()
+    annotations['CL_Label'] = annotations['RIBCA_CellType'].map(cl_label_map)
+    annotations['CL_ID'] = annotations['RIBCA_CellType'].map(cl_id_map)
+    annotations['CL_ID'] = annotations['CL_ID'].fillna('CL:0000000')
+
+    return annotations
+
+
+def create_cell_type_manifest(df, outdir):
+    cell_type_manifest_dict = {}
+
+    for column_header in ['RIBCA_CellType', 'CL_ID']:
+        sub_dict = {
+            val: int((df[column_header] == val).sum())
+            for val in df[column_header].unique()
+        }
+        # Remove NaN key if it exists
+        sub_dict = {k: v for k, v in sub_dict.items() if not pd.isna(k)}
+        cell_type_manifest_dict[column_header] = sub_dict
+
+    with open(f'{outdir}/cell_type_manifest.json', 'w') as f:
+        json.dump(cell_type_manifest_dict, f)
 
 
 def read_ribca_output(
-    results_dir: Path, cell_type_mapping: dict[str, tuple[str, str]]
+    results_dir: Path
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
     annotations = pd.read_csv(
         results_dir / "headless_annotation_0.txt",
@@ -36,11 +59,7 @@ def read_ribca_output(
         index_col=0,
     )
 
-    # TODO: reconsider mapping structure
-    annotations["RIBCA_CO_Label"] = [
-        cell_type_mapping[ct][0] for ct in annotations["RIBCA_CellType"]
-    ]
-    annotations["RIBCA_CO_ID"] = [cell_type_mapping[ct][1] for ct in annotations["RIBCA_CellType"]]
+    annotations = map_to_clid(annotations)
 
     df = pd.concat([annotations, confidence, thresholds], axis=1).sort_index()
 
@@ -58,8 +77,6 @@ def read_ribca_output(
 
 
 def convert_ribca_output(results_dir: Path):
-    cell_type_mapping = read_cell_type_mapping()
-
     ribca_results_dir = Path("ribca_results")
     with open(results_dir / "image_name.txt") as f:
         image_name = f.read().strip()
@@ -67,7 +84,9 @@ def convert_ribca_output(results_dir: Path):
     ribca_results_subdir.mkdir(exist_ok=True, parents=True)
     copytree(results_dir, ribca_results_subdir, dirs_exist_ok=True)
 
-    df, votes_df = read_ribca_output(results_dir, cell_type_mapping)
+    df, votes_df = read_ribca_output(results_dir)
+    create_cell_type_manifest(df, ribca_results_subdir)
+    #!TODO! should i cite hra somewhere here
     print(
         "Writing results in HDF5 format to",
         (hdf5_path := ribca_results_subdir / "ribca_results.hdf5"),
@@ -79,7 +98,7 @@ def convert_ribca_output(results_dir: Path):
     sprm_dir = Path("ribca_for_sprm")
     sprm_dir.mkdir(exist_ok=True, parents=True)
     print("Writing CSV annotation results to", (csv_path := sprm_dir / f"{image_name}.csv"))
-    df["RIBCA_CO_ID"].to_csv(csv_path)
+    df["CL_ID"].to_csv(csv_path)
 
 
 if __name__ == "__main__":
